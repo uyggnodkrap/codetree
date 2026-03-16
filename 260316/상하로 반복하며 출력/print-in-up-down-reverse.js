1f7a0d2cda5e78d0b33c49ce2ff958423d2c1bea
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 21:01:17 +0900

    Title: 상하로 반복하며 출력, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

--- 260316/상하로 반복하며 출력/print-in-up-down-reverse.js
+++ 260316/상하로 반복하며 출력/print-in-up-down-reverse.js
@@ -2,19 +2,11 @@ const fs = require("fs")
 
 const n = Number(fs.readFileSync(0).toString().trim())
 
-
-
 const answer = Array.from({length: n}, () => new Array(n).fill(0))
 
 for (let i = 0; i < n; i++) {
-    let asc = 1
-    let desc = n
     for (let j = 0; j < n; j++) {
-        if (i % 2 == 0) {
-            answer[j][i] = asc++
-        } else {
-            answer[j][i] = desc--
-        }
+        answer[j][i] = i % 2 === 0 ? j + 1 : n - j
     }
 }
 
