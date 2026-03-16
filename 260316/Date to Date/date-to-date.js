e517e61755c40c478c9b66b219e2e6257d539d72
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 22:43:07 +0900

    Title: Date to Date, Time: 69ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260316/Date to Date/date-to-date.js
+++ 260316/Date to Date/date-to-date.js
@@ -8,7 +8,7 @@ const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 const month = [0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 const getDate = (x, y) => {
-    return month.slice(1, x).reduce((acc, cur) => acc + cur, y)
+    return month.slice(0, x).reduce((acc, cur) => acc + cur, y)
 }
 
 console.log(getDate(m2, d2) - getDate(m1, d1))
