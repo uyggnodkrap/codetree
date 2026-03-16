f8f778a6c1b8db6bf5eb8b7e49f18a0dad29de11
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 22:55:33 +0900

    Title: DateTime to DateTime, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260316/DateTime to DateTime/datetime-to-datetime.js
+++ 260316/DateTime to DateTime/datetime-to-datetime.js
@@ -5,14 +5,9 @@ const [A, B, C] = input[0].split(' ').map(Number);
 
 // Please write your code here.
 
-const getDate = (m, d) => {
-    const month = [0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    return month.slice(1, m).reduce((acc, cur) => acc + cur, 0) 
-}
+const month = (A - 11) * 24 * 60
+const day = (B - 11) * 60
+const min = C - 11
 
-let month = (A - 11) * 24 * 60
-let day = (B - 11) * 60
-let min = C - 11
-
-let answer = month + day + min
+const answer = month + day + min
 console.log(answer < 0 ? - 1 : answer)
