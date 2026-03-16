463dbcfdb45c77c4503c31523ac0f6da82c06d36
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 22:45:15 +0900

    Title: Date to Date, Time: 74ms, Memory: 10MB, Status: Passed - Codetree

--- 260316/Date to Date/date-to-date.js
+++ 260316/Date to Date/date-to-date.js
@@ -5,10 +5,9 @@ const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 
 // Please Write your code here.
 
-const month = [0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-const getDate = (x, y) => {
-    return month.slice(0, x).reduce((acc, cur) => acc + cur, y)
+const getDate = (m, d) => {
+    const month = [0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return month.slice(1, m).reduce((acc, cur) => acc + cur, d) 
 }
 
-console.log(getDate(m2, d2) - getDate(m1, d1))
+console.log(getDate(m2, d2) - getDate(m1, d1) + 1)
