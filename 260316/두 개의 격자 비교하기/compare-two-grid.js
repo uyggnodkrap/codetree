b8e47e2f0801c24f2d3162103282490e78818939
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 19:57:55 +0900

    Title: 두 개의 격자 비교하기, Time: 65ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260316/두 개의 격자 비교하기/compare-two-grid.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+
+const input = fs.readFileSync(0).toString().trim().split('\n')
+
+const [n, m] = input[0].split(' ').map(Number)
+
+const arr1 = input.slice(1,n+1).map(i => i.split(' ').map(Number))
+const arr2 = input.slice(n+1).map(i => i.split(' ').map(Number))
+
+const answer = Array.from({length: n}, () => new Array(m).fill(0))
+
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j ++) {
+        if (arr1[i][j] !== arr2[i][j]) {
+            answer[i][j] = 1
+        }
+    }
+}
+
+for (let i = 0; i < m; i++) {
+    console.log(answer[i].join(' '))
+}
