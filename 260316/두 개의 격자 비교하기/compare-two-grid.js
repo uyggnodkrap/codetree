5109ffc2f36fc70c8416ffb7990fc6144d435e43
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 20:02:00 +0900

    Title: 두 개의 격자 비교하기, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260316/두 개의 격자 비교하기/compare-two-grid.js
+++ 260316/두 개의 격자 비교하기/compare-two-grid.js
@@ -7,16 +7,8 @@ const [n, m] = input[0].split(' ').map(Number)
 const arr1 = input.slice(1,n+1).map(i => i.split(' ').map(Number))
 const arr2 = input.slice(n+1).map(i => i.split(' ').map(Number))
 
-const answer = Array.from({length: n}, () => new Array(m).fill(0))
+// const answer = Array.from({length: n}, () => new Array(m).fill(0))
 
 for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j ++) {
-        if (arr1[i][j] !== arr2[i][j]) {
-            answer[i][j] = 1
-        }
-    }
-}
-
-for (let i = 0; i < n; i++) {
-    console.log(answer[i].join(' '))
+    console.log(arr1[i].map((v, j) => v !== arr2[i][j] ? 1 : 0).join(' '))
 }
