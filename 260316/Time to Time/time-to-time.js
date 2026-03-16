202406daf6f7262a0181d924d45c1f4e20550695
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 22:07:09 +0900

    Title: Time to Time, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260316/Time to Time/time-to-time.js
+++ 260316/Time to Time/time-to-time.js
@@ -2,10 +2,4 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const [a, b, c, d] = input[0].split(' ').map(Number);
 // Please Write your code here.
-
-let answer = 0;
-
-answer += (c - a) * 60
-answer += (d - b)
-
-console.log(answer)
+console.log(`${(c - a) * 60 + (d - b)}`)
