8253a8d826ba1c7a824a20e87f3552d85ec10f7f
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-16 20:56:03 +0900

    Title: 상하로 반복하며 출력, Time: 66ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260316/상하로 반복하며 출력/print-in-up-down-reverse.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+
+const n = Number(fs.readFileSync(0).toString().trim())
+
+
+
+const answer = Array.from({length: n}, () => new Array(n).fill(0))
+
+for (let i = 0; i < 4; i++) {
+    let asc = 1
+    let desc = n
+    for (let j = 0; j < 4; j++) {
+        if (i % 2 == 0) {
+            answer[j][i] = asc++
+        } else {
+            answer[j][i] = desc--
+        }
+    }
+}
+
+for (let i = 0; i < n; i++) {
+    console.log(answer[i].join(''))
+}
