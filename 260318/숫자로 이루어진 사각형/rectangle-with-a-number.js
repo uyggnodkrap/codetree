99ccd7178388f4b07a2653afa0e99092a3468013
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-18 19:39:54 +0900

    Title: 숫자로 이루어진 사각형, Time: 89ms, Memory: 11MB, Status: Passed - Codetree

--- 260318/숫자로 이루어진 사각형/rectangle-with-a-number.js
+++ 260318/숫자로 이루어진 사각형/rectangle-with-a-number.js
@@ -3,10 +3,12 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 // Please write your code here.
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let x = ""
 let cnt = 0
 for (let i = 0; i < n ** 2; i++) {
-    let value = (i % 9) + 1
+    let idx = i % 9  
+    let value = arr[idx]
     x += value + ' '
     cnt++
     if (cnt == n) {
