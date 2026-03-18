53cc755fc2ffbafb9d796deb843745e47cf32b12
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-18 19:03:37 +0900

    Title: 반복 출력하기, Time: 75ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260318/반복 출력하기/repeated-output.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+// Please Write your code here.
+
+const x = "12345^&*()_"
+
+for (let i = 0; i < n; i++) console.log(x)
