16c98f798cada41c7f62392f6a725385105ca3c2
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-03-29 14:08:36 +0900

    Title: 줄 세우기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260329/줄 세우기 2/line-up-students-2.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+// Please Write your code here.
