b9cdf64df2336a96710db235f96f54e037cc5426
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-05-13 01:09:02 +0900

    [Passed] 재귀함수를 이용한 별 출력 (62ms, 10MB)

--- 260513/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
+++ 260513/재귀함수를 이용한 별 출력/star-output-with-recursive-function.js
@@ -4,12 +4,14 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let n = Number(input[0]);
 // Please Write your code here.
 
+let result = ''
+
 const recursive = (x) => {
     if (x === 0) return 
-    const arr = []
-    for (let i = 0; i < n - x + 1; i++) arr.push('*')
-    console.log(arr.join(''))
     recursive(x-1)
+    result += "*".repeat(x) + "\n"
 }
 
+
 recursive(n)
+console.log(result)
