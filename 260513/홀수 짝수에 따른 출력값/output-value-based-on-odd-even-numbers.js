3465a5a7f95621c255b1e614783d6a6a06d43cec
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-05-13 01:51:56 +0900

    [Passed] 홀수 짝수에 따른 출력값 (65ms, 10MB)

---
+++ 260513/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+
+// Please Write your code here.
+
+const recursive = (x) => {
+    if (x <= 2) return x
+    return recursive(x - 2) + x
+}
+
+console.log(recursive(n))
