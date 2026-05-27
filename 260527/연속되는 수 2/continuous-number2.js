92745c43a84b3d3f8412b6c7fc197210bb80324c
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-05-27 23:59:59 +0900

    [Passed] 연속되는 수 2 (68ms, 10MB)

--- 260527/연속되는 수 2/continuous-number2.js
+++ 260527/연속되는 수 2/continuous-number2.js
@@ -5,13 +5,12 @@ const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
 // Please Write your code here.
-let answer = 0
-let cnt = 1
+let answer = 0, cnt = 1
+
 for (let i = 0; i < n; i++) {
     if (arr[i] == arr[i-1]) {
         cnt++
     } else {
-        answer = Math.max(answer, cnt)
         cnt = 1
     }
     answer = Math.max(answer, cnt)
