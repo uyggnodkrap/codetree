3ca2394d03b7acd657bf1effbc7b2fb18afb2224
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2026-09-14 18:08:59 +0900

    [Passed] 정수의 조건 여부 (127ms, 10MB)

---
+++ 260914/정수의 조건 여부/numbers-condition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x >= 113) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        
+    }
+}
